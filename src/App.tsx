01f012d087de20427016b0bb42f1cf4ba57f9d44
giorgigio1 <giorgigiorgdze93@gmail.com>
2023-03-21 09:20:47 +0400

    first commit1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dolar from "./images/dolar.png";
+import Person from "./images/person.svg";
+import Logo from "./images/logo.svg";
+import "./App.css";
+import { Button } from "./components/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+import { mdiCurrencyUsd } from "@mdi/js";
+import { Icon } from "@mdi/react";
 
 function App() {
+  const handleReset = () => {
+    console.log("Reset");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <figure className="logoFigure">
+      <img className="logo" src={Logo} alt="" />
+      </figure>
+      <div className="main">
+        <section className="left">
+          <div className="top">
+            <p>Bill</p>
+            <input placeholder="0" className="priceInput" type="text" />
+          </div>
+          <div className="middle">
+            <p>Select Tip %</p>
+            <div className="buttons">
+              <button>5%</button>
+              <button>10%</button>
+              <button>15%</button>
+              <button>25%</button>
+              <button>50%</button>
+              <input placeholder="Custom" className="customInput" type="text" />
+            </div>
+          </div>
+          <div className="bottom">
+            <p>Number of People</p>
+            <input placeholder="0" className="numberOfpeople" type="text" />
+          </div>
+        </section>
+        <section className="right">
+          <div className="tipAmountContent">
+            <div className="textSide">
+              <p>Tip Amount</p>
+              <span>/ person</span>
+            </div>
+            <div className="valueSide">
+              <FontAwesomeIcon icon={faDollarSign} size="2x" color="#26C2AE" />
+              <p>0.00</p>
+            </div>
+          </div>
+          <div className="tipAmountContent tipAmountContent2">
+            <div className="textSide">
+              <p>Total</p>
+              <span>/ person</span>
+            </div>
+            <div className="valueSide">
+              <FontAwesomeIcon icon={faDollarSign} size="2x" color="#26C2AE" />
+              <p>0.00</p>
+            </div>
+          </div>
+          <Button children={"RESET"} onClick={handleReset} />
+        </section>
+      </div>
     </div>
   );
 }
